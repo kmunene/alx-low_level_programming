bb6aa4dd8a172dba194cbd5ad0abeaa5f09c1e4a
kmunene <kmunene529@gmail.com>
2023-06-18 11:58:06 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Entry point of the program
  *
- * Return: Always 1 (error)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-
-	write(STDERR_FILENO, message, len);
-
-	return (1);
+          write(2,
+           "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+         
+	  return (1);
 }
