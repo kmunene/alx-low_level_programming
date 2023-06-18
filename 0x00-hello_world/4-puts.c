4a75eec1b4a3936871cb0c528af273f471e5c2ec
kmunene <kmunene529@gmail.com>
2023-06-18 10:35:00 -0700

    trial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * calling a function
+ */
 
-int main() {
-  puts(R"("Programming is like building a multilingual puzzle)\n");
+int main(void) {
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
-}
+} 
