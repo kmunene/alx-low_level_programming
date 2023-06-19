bf961a3f0f7767e761ff9f68d553f4126fb66840
kmunene <kmunene529@gmail.com>
2023-06-19 13:28:48 -0700

    dd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,30 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int tensDigit1;
-	int onesDigit1;
-	int tensDigit2;
-	int onesDigit2;
+int main() {
+  /* Declare variables. */
+  int digits[3] = {0, 1, 2};
+  int i, j, k;
 
-	for (tensDigit1 = 0; tensDigit1 < 10; tensDigit1++)
-	{
-		for (onesDigit1 = 0; onesDigit1 < 10; onesDigit1++)
-		{
-			for (tensDigit2 = tensDigit1; tensDigit2 < 10; tensDigit2++)
-			{
-				for (onesDigit2 = (tensDigit2 == tensDigit1 ? onesDigit1 + 1 : 0); onesDigit2 < 10; onesDigit2++)
-				{
-					putchar('0' + tensDigit1);
-					putchar('0' + onesDigit1);
-					putchar(' ');
-					putchar('0' + tensDigit2);
-					putchar('0' + onesDigit2);
+  /* Iterate over all possible combinations of three digits. */
+  for (i = 0; i < 3; i++) {
+    for (j = i + 1; j < 3; j++) {
+      for (k = j + 1; k < 3; k++) {
+        /* Check if the three digits are different. */
+        if (digits[i] != digits[j] && digits[i] != digits[k] && digits[j] != digits[k]) {
+          /* Print the three digits, separated by a comma and a space. */
+          putchar(digits[i] + '0');
+          putchar(',');
+          putchar(' ');
+          putchar(digits[j] + '0');
+          putchar(',');
+          putchar(' ');
+          putchar(digits[k] + '0');
+          putchar('\n');
+        }
+      }
+    }
+  }
 
-					if (tensDigit1 != 9 || onesDigit1 != 8 || tensDigit2 != 9 || onesDigit2 != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+  /* Return 0. */
+  return 0;
 }
-
