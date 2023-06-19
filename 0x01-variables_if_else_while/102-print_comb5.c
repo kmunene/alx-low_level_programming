f938d83bbbaf6a29b361dd862c4216ed7a53d2e4
kmunene <kmunene529@gmail.com>
2023-06-19 13:25:39 -0700

    ff

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,28 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int tensDigit1;
-	int onesDigit1;
-	int tensDigit2;
-	int onesDigit2;
+void print_combinations(int digits[3]) {
+  int i, j, k;
 
-	for (tensDigit1 = 0; tensDigit1 < 10; tensDigit1++)
-	{
-		for (onesDigit1 = 0; onesDigit1 < 10; onesDigit1++)
-		{
-			for (tensDigit2 = 0; tensDigit2 < 10; tensDigit2++)
-			{
-				for (onesDigit2 = 0; onesDigit2 < 10; onesDigit2++)
-				{
-					if (tensDigit1 < tensDigit2 || (tensDigit1 == tensDigit2 && onesDigit1 < onesDigit2))
-					{
-						putchar('0' + tensDigit1);
-						putchar('0' + onesDigit1);
-						putchar(' ');
-						putchar('0' + tensDigit2);
-						putchar('0' + onesDigit2);
-
-						if (!(tensDigit1 == 9 && onesDigit1 == 8 && tensDigit2 == 9 && onesDigit2 == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+  for (i = 0; i < 3; i++) {
+    for (j = i + 1; j < 3; j++) {
+      for (k = j + 1; k < 3; k++) {
+        if (digits[i] != digits[j] && digits[i] != digits[k] && digits[j] != digits[k]) {
+          putchar(digits[i] + '0');
+          putchar(digits[j] + '0');
+          putchar(digits[k] + '0');
+          putchar(',');
+          putchar(' ');
+        }
+      }
+    }
+  }
 }
 
+int main() {
+  int digits[3] = {0, 1, 2};
+
+  /* Print all possible different combinations of three digits. */
+  print_combinations(digits);
+
+  return 0;
+}
