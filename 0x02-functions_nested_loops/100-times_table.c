928820e7b19b6f4e6bb57f956f1106694e8402e1
kmunene <kmunene529@gmail.com>
2023-06-21 15:00:35 -0700

    all done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,59 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
-
 	if (n >= 0 && n <= 15)
 	{
+		int i, j;
+
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
+			print_times_row(i, n);
 			_putchar('\n');
+		}
+	}
+}
+
+/**
+ * print_times_row - prints a single row of the times table
+ * @row: the row number
+ * @n: number of the times table
+ */
+void print_times_row(int row, int n)
+{
+	int j;
+
+	for (j = 0; j <= n; j++)
+	{
+		int result = row * j;
+
+		if (j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+			}
+		}
+
+		if (result < 10)
+			_putchar(result + '0');
+		else if (result < 100)
+		{
+			_putchar(result / 10 + '0');
+			_putchar(result % 10 + '0');
+		}
+		else
+		{
+			_putchar(result / 100 + '0');
+			_putchar((result / 10) % 10 + '0');
+			_putchar(result % 10 + '0');
 		}
 	}
 }
