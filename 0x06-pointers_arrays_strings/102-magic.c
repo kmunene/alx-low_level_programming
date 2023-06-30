ddd4b8b279aa99bacdbee6b39727c1417086496c
kmunene <kmunene529@gmail.com>
2023-06-30 11:36:21 -0700

    second trial

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,16 +2,14 @@
 
 int main(void)
 {
-    int n;
-    int a[5];
-    int *p;
+  int n;
+  int a[5];
+  int *p;
 
-    a[2] = 1024;
-    p = &n;
-
-    *(p + (sizeof(int) * 2 / sizeof(*p))) = 98;
-
-    printf("a[2] = %d\n", a[2]);
-    return 0;
+  a[2] = 1024;
+  p = &n;
+  *(p + 2) = 98;
+  
+  printf("a[2] = %d\n", a[2]);
+  return 0;
 }
-
