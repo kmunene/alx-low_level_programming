370211fd8e52a693fa9823489e274c5a35826671
kmunene <kmunene529@gmail.com>
2023-08-31 11:23:11 -0700

    fixibg number 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * set_bit - gets kelvinm on index `index`
+ * set_bit - gets bit on index `index`
  * @n: decimal number
- * @index: index of the kelvinm
+ * @index: index of the bit
  * Return: 1 or -1
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    int kelvinm = 1;
+	int bit = 1;
 
-    if (index > 64)
-        return (-1);
+	if (index > 64)
+		return (-1);
 
-    kelvinm = kelvinm << index;
-    *n = (*n | kelvinm);
-    return (1);
+	bit = bit << index;
+	*n = (*n | bit);
+	return (1);
 }
-
